7aa89be1236b05f729c095a3c3cf2721c6013f77
Jessica Nam <jessicanam@nevada.unr.edu>
2022-12-13 04:23:25 -0800

    Code cleanup

--- database.py
+++ database.py
@@ -1,110 +1,104 @@
-from bs4 import BeautifulSoup
-from requests.compat import urljoin
-import requests
-import csv
-import re
+from bs4 import BeautifulSoup # For webscraping
+from pprint import pprint     # For readability while testing
+import requests               # For accessing URL
+import csv                    # For writing data
+import re                     # For extracting postID 
 
-# Page we want to scrape (Questions page from stackoverflow - filtered to tags)
-URL = "https://stackoverflow.com/questions/tagged/"
-PAGE_LIMIT = 1
-LANGUAGE = "python"
-TAB = "votes" # tab can be votes, newest, recent activity, most frequent - later feature
+# These are the base URLs I will use
+URL1 = "https://stackoverflow.com/questions/tagged/"
+URL2 = "https://stackoverflow.com/questions/" # For getting answer URL
+
+# This is a fully configured example URL for votes and newest
+# URL = "https://stackoverflow.com/questions/tagged/python?tab=votes&page=1"
+# URL = "https://stackoverflow.com/questions/tagged/recursion?tab=newest&page=1"
+
+# Users should be able to filter what kind of StackOverflow Questions page the chatbot will extract data from
+# Choices (default is no tag and newest tab)
+TAG = ""  # can be "python", "recursion". etc.
+TAB = "votes"  # can be "newest", "votes", "Frequent (Questions with most links)"
+
+# Pre set page amount
+PAGE_LIMIT = 3 # if 3 it actually grabs 10 pages idky
 
 
-def build_url(base_url=URL, tab=TAB, page=1):
-    """ Builds StackOverflow questions URL format which takes in two parameters: tab and page
-        Example: https://stackoverflow.com/questions/tagged/python?tab=votes&page=1"""
+def build_url(base_url=URL1, tag = TAG, tab=TAB, page=PAGE_LIMIT):
+    """ Builds StackOverflow questions URL format which takes in two parameters: tab and page """
 
-    return f"{base_url}{LANGUAGE}?tab={tab}&page={page}"
+    return f"{base_url}{tag}?tab={tab}&page={page}"
 
-def build_answer_url(base_url=URL, postID=""):
-    """ Builds StackOverflow answer URL format which takes in two parameters: postID and question
-        Example: https://stackoverflow.com/questions/74701549/is-there-any-documentation-on-the-typescript-type-spread-behavior"""
-    return f"https://stackoverflow.com/questions/{postID}"
-
+def build_answer_url(base_url=URL2, postID=""):
+    """ Builds StackOverflow answer URL format which takes in two parameters: postID and question """    
+        
+    return f"{base_url}{postID}"
 
 def scrape_one_question_page(page=1):
-    """ Retrives newest question and answers from StackOverflow by scraping one page 
-        *** NOTE TO SELF: "answers" derived from this function only indicates answer count """
+    """ Retrives newest question, postID, votes, answer, view count from StackOverflow by scraping one page 
+        NOTE TO SELF: "answers" derived from this function only indicates answer count """
 
     response = requests.get(build_url(page=page))
+
     # Parse HTML with BeautifulSoup
     soup = BeautifulSoup(response.text, features="html.parser")
 
-
-    # Questions found in h3 tags with class='s-post-summary--content-title'
+    ##### Questions found in h3 tags with class='s-post-summary--content-title'
     questions_list = soup.find_all(
         "h3", class_="s-post-summary--content-title")
 
+    ##### Find question answer link (we can grab postID from this)
+    answers_link_list = soup.find_all("a", class_="s-link")
+    answers_link_list = answers_link_list[3:]   # Remove the first three links which are javascript:void(0)
+    answers_link_list = answers_link_list[:-1]  # Remove last link which is https://stackexchange.com/questions?tab=hot
 
+    ##### Vote/Answer/View descriptions found in span tags with class='s-post-summary--stats-item-number'
+    vote_answer_view_HTML_list = soup.find_all( 
+        "span", class_="s-post-summary--stats-item-number") # THIS IS HTML NOT TEXT
+    vote_answer_view_list = []
+    for v in vote_answer_view_HTML_list:
+        vote_answer_view_list.append(v.text) # THIS IS TEXT
+
+    ##### CURRENTLY UNUSED ######
     # Question descriptions found in h3 tags with class='s-post-summary--content-excerpt'
     description_list = soup.find_all(
         "h3", class_="s-post-summary--content-excerpt")
 
 
-    # Vote/Answer/View descriptions found in span tags with class='s-post-summary--stats-item-number'
-    # (Vote,Answer,View) format
-    vote_answer_view_HTML_list = soup.find_all(
-        "span", class_="s-post-summary--stats-item-number") # THIS IS HTML NOT TEXT
 
-
-    # (Vote,Answer,View) values
-    vote_answer_view_list = []
-    for v in vote_answer_view_HTML_list:
-        vote_answer_view_list.append(v.text) # THIS IS TEXT
-
-    # Find link
-    answers_link_list = soup.find_all("a", class_="s-link")
-    
-
-    # Remove the first three links which are javascript:void(0)
-    answers_link_list = answers_link_list[3:]
-
-    # Remove last link which is https://stackexchange.com/questions?tab=hot
-    answers_link_list = answers_link_list[:-1]
-
-    # (Question,Vote,Answer,View)
+    # [Question, Post_ID, Vote, Answer, View]
     OnePageOutput = []
 
     i = 0
     for (x, y) in zip(questions_list, answers_link_list):
         OnePageOutput.append(x.text.strip())
+
+        ### Grab question summary 
         question = x.text.strip()
 
-
+        ### Grab postID
         link = y['href']
-        post_ID = link[10:19]
-        post_ID = str(re.findall('/([^"]*)/', post_ID))
-        post_ID = post_ID[1:-1]
-        post_ID = post_ID.replace("'", "")
-        print(post_ID)
-        print(type(post_ID))
-        #post_ID = post_ID[:post_ID.index("/")]
-        OnePageOutput.append(post_ID)
+        post_ID = link[10:20]                           # format: "/503093/"
+        post_ID = str(re.findall('/([^"]*)/', post_ID)) # format: "['503093']"
+        post_ID = post_ID[1:-1]                         # format: "'503093'"
+        post_ID = post_ID.replace("'", "")              # format: "503093"
+        OnePageOutput.append(post_ID)                   # Add as second element after its question summary
 
     
+        ### Grab [Vote, Answer, View] values for the question 
         for j in range(3):
             OnePageOutput.append(vote_answer_view_list[i+j])
 
         i += j+1
-  
     i = 0
-    # List values ([Question,Vote,Answer,View]) split for each question
-    triplet_vote_answer_view_list = [
+
+    # data values ([Question,PostID,Vote,Answer,View]) split for each question
+    data = [
     OnePageOutput[i:i + 5] for i in range(0, len(OnePageOutput), 5)]
 
     # Dictionary format for CSV
     QuestionPage = []
 
-    for i in triplet_vote_answer_view_list:
-        # Unpack values
-
+    # Unpack data into dictionary format
+    for i in data:
         question, postID, vote, answer, view = [str(e) for e in i]
-
-        # if answer > 0 and vote > 0: # Good answer
-        #     pass
-        # elif answer > 0: # Okay answer
-        #     pass
 
         QuestionPage.append({
             "question": question,
@@ -117,7 +111,7 @@ def scrape_one_question_page(page=1):
 
 
 def scrape_question_pages(page_limit):
-    """ This function can scrape multiple pages limited to page_limit """
+    """ This function can scrape multiple pages up tp page_limit """
     questions = []
     for i in range(1, page_limit + 1):
         page_question = scrape_one_question_page(i)
@@ -126,8 +120,14 @@ def scrape_question_pages(page_limit):
 
 
 def export_data():
-    # data = scrape_one_question_page(2)
-    data = scrape_question_pages(100)
+    """ Export dictionary data into questions.csv file """
+
+    ### If you only want data from one page ###
+    # data = scrape_one_question_page(PAGE_LIMIT)
+
+    ### If you want data from multiple pages ###
+    data = scrape_question_pages(PAGE_LIMIT)
+
     with open("questions.csv", "w") as f:
         fieldnames = ["question", "postID", "votes", "answers", "views"] # Rows for CSV
         writer = csv.DictWriter(f, fieldnames=fieldnames)
@@ -153,12 +153,12 @@ def scrape_one_answer_page(postID):
 
 if __name__ == "__main__":
 
-    from pprint import pprint # For readability
-    L = scrape_one_question_page(1)
+    L = scrape_one_question_page(2)
+    print(L)
     print(L[0]["postID"])
     print(L[0]["question"])
 
-    print(build_answer_url(URL, L[0]["postID"]))
+    print(build_answer_url(URL1, L[0]["postID"]))
     # scrape_one_answer_page(L[1]["postID"])
     #scrape_one_answer_page(36730372, "extract-the-text-from-p-within-div-with-beautifulsoup")
     export_data()
