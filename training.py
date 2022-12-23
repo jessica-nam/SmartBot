ecc81f56bb12498665bf32625ceb3a9875622704
Jessica Nam <jessicanam@nevada.unr.edu>
2022-12-23 00:50:27 -0800

    Changed webscrape to JSON to load more data for ML model

--- training.py
+++ training.py
@@ -12,7 +12,7 @@ from keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer()
 
-intents = json.loads(open('intents.json').read()) # Reading contents of JSON file as text into JSON object intents (dictionary)
+intents = json.loads(open('test.json').read()) # Reading contents of JSON file as text into JSON object intents (dictionary)
 
 words = []
 classes = []
@@ -33,8 +33,23 @@ for intent in intents['intents']:
 
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = sorted(set(words))
+
 classes = sorted(set(classes))
 
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(words, open('classes.pkl', 'wb'))
 
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = [] # empty bag of words
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+print(documents)
